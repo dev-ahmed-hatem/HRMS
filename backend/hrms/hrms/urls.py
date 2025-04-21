cbfe7316e8dea2ef4fc05544f032d99d63f3d6ff
dev-ahmed-hatem <ahmedhatemezzathelal@gmail.com>
2025-04-22 00:06:50 +0200

    initialzed backend (settings, users, authentication)

---
+++ backend/hrms/hrms/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include([
+        path('auth/', include('authentication.urls')),
+        path('users/', include('users.urls'))
+    ])),
+]

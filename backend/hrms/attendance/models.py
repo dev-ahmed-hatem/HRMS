212d46264999f969a80384b60c3cb5ada4347d15
dev-ahmed-hatem <ahmedhatemezzathelal@gmail.com>
2025-06-29 05:36:48 +0300

    implemented attendance settings models (with default data signal)

--- backend/hrms/attendance/models.py
+++ backend/hrms/attendance/models.py
@@ -50,3 +50,41 @@ class Attendance(models.Model):
             raise ValidationError({
                 'check_out': _("وقت الانصراف لا يمكن أن يكون قبل وقت الحضور.")
             })
+
+
+class Day(models.Model):
+    class DayEnum(models.TextChoices):
+        SATURDAY = "saturday", _("السبت")
+        SUNDAY = "sunday", _("الأحد")
+        MONDAY = "monday", _("الاثنين")
+        TUESDAY = "tuesday", _("الثلاثاء")
+        WEDNESDAY = "wednesday", _("الأربعاء")
+        THURSDAY = "thursday", _("الخميس")
+        FRIDAY = "friday", _("الجمعة")
+
+    name = models.CharField(max_length=10, choices=DayEnum.choices, unique=True)
+
+    def __str__(self):
+        return self.get_name_display()
+
+    class Meta:
+        verbose_name = _("اليوم")
+        verbose_name_plural = _("الأيام")
+        ordering = ['id']
+
+
+class AttendanceSettings(models.Model):
+    name = models.CharField(_("الاسم"), max_length=100, blank=True, null=True)
+
+    check_in = models.TimeField(_("وقت الحضور"))
+    check_out = models.TimeField(_("وقت الانصراف"))
+    grace_period = models.PositiveIntegerField(_("فترة السماحية"), default=15)
+
+    working_days = models.ManyToManyField("Day", verbose_name=_("أيام العمل"))
+
+    def __str__(self):
+        return self.name or _("إعدادات الحضور الافتراضية")
+
+    class Meta:
+        verbose_name = _("إعدادات الحضور")
+        verbose_name_plural = _("إعدادات الحضور")
